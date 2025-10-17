7e1245759dda4923b3cb601530daa0ceb2b60206
Bharat Jain <bharat.jain@plaksha.edu.in>
2025-10-17 19:16:48 +0530

    batching and tokenization of data

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,6 @@
 import torch 
 import tiktoken
+import random
 
 '''
 Steps: 
@@ -21,15 +22,17 @@ class Nano_GPT:
             self.device = "cpu"
 
         self.block_size = 8 
-        self.batch_size = 4
+        self.batch_size = 10
         self.data_size = len(self.data)
         self.enc = tiktoken.get_encoding("o200k_base")
 
+        self.data_tokenized = torch.tensor(self.enc.encode(self.data)).to(self.device)
 
         #90-10 train val split
         split = int(self.data_size*0.90)
         train_data = self.data[:split]
         val_data = self.data[split:]
+        
     
     def _load_dataset(self,path=None): 
         data = "input.txt" if path is None else path
@@ -40,6 +43,7 @@ class Nano_GPT:
         self.vocab_size = len(self.s_vocab)
         self.vocab_map = {c:ind for ind,c in enumerate(self.s_vocab)}
         print(f"Size of the vocab {self.vocab_size}\n Elements: {self.s_vocab}")
+        
     
     def simple_encoding(self,input_text): 
         ## Using a simple encoding based on the charater index --> character mapping and vice_versa 
@@ -51,6 +55,17 @@ class Nano_GPT:
         decoded = "".join([self.s_vocab[i] for i in encoded_text])
         return decoded
 
+    def get_batch(self): 
+        ## Get one batch of the shakespeare dataset, across two dimensions: Batch dimension and time dimension
+        ## One batch is on a random snippet of the data for each batch here
+
+        start_points = torch.randint(0,self.vocab_size-self.block_size,(self.batch_size,))
+        print(start_points)
+        X = torch.stack([self.data_tokenized[i:i+self.block_size] for i in start_points])
+        y = torch.stack([self.data_tokenized[i+1:i+self.block_size+1] for i in start_points])
+        return X,y
+        
+        
 
     def tokenize(self,input_text): 
         ## Tokenization ==> Using Tiktoken
@@ -63,7 +78,7 @@ def main():
     gpt = Nano_GPT()
     encoded = gpt.simple_encoding("Bharat Jain")
     print(encoded, "\n", gpt.simple_decoding(encoded))
-
+    print(gpt.get_batch())
 if __name__ == "__main__": 
     main()
 
