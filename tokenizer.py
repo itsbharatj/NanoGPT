2bffadd06d2c5cdf921f9b81bbaef332f8e0fa44
Bharat Jain <bharat.jain@plaksha.edu.in>
2025-10-23 14:04:13 +0530

    token and positional encoding

--- tokenizer.py
+++ tokenizer.py
@@ -1,20 +1,79 @@
 import torch 
 import tiktoken
 import random
-
+import torch.nn as nn 
+from torch.nn import functional as F 
+torch.manual_seed(1337)
+import torch
 '''
 Steps: 
 1. Load the dataset 
 2. Batching and making blocks of the dataset
 3. Make int
 '''
+if torch.cuda.is_available(): 
+    device = "cuda" 
+elif torch.backends.mps.is_available(): 
+    device = "mps" 
+else: 
+    device = "cpu"
+
+block_size = 8 
+batch_size = 32
+n_embed = 32
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self,vocab_size): 
+        super().__init__()
+
+        self.token_embedding_table = nn.Embedding(vocab_size,n_embed)
+        self.position_embedding_table = nn.Embedding(block_size,n_embed)
+        self.lm_head = nn.Linear(n_embed,vocab_size)
+    
+    def forward(self, idx, target=None): 
+        B,T = idx.shape
+
+        tok_emd = self.token_embedding_table(idx) ## Put the embeddings from this batch (B,T,C->vocab_size) ==> (4,8,64)
+        pos_emd = self.position_embedding_table(torch.arange(T,device=device)) ## Creates an (T,C)
+        x = tok_emd+pos_emd
+        logits = self.lm_head(x) # (B,T,C)
+        if target is None: 
+            loss = None
+        else: 
+            B,T,C = logits.shape
+            logits_R = logits.view(B*T,C)
+            target = target.view(B*T)
+
+            loss = F.cross_entropy(logits_R,target)
+
+        return logits,loss
+
+    def generate(self,idx,max_num_tokens): 
+        for _ in range(max_num_tokens): 
+            ## Get the predictions: 
+            logits,loss = self(idx) ## runs the forward functions by default
+
+            ## focus only on the last token: 
+            logits = logits[:,-1,:]
+
+            ## apply softmax to get probs: 
+            probs = F.softmax(logits,dim=1) ## (B,C)
+
+            ## Sample from the distribution: 
+            idx_next = torch.multinomial(probs,num_samples=1) ## Does it also give the original one?
+
+            ## Append the sampled idx to the running sequence: 
+            idx = torch.cat((idx,idx_next),dim=1)
+        
+        return idx
+
 
 class Nano_GPT: 
     def __init__(self):
         self._load_dataset()
 
         ## Constants: 
-        if torch.cuda.is_available(): 
+        if torch.cuda.is_available():
             self.device = "cuda" 
         elif torch.backends.mps.is_available(): 
             self.device = "mps" 
@@ -22,11 +81,21 @@ class Nano_GPT:
             self.device = "cpu"
 
         self.block_size = 8 
-        self.batch_size = 10
+        self.batch_size = 32
         self.data_size = len(self.data)
-        self.enc = tiktoken.get_encoding("o200k_base")
+        # self.enc = tiktoken.get_encoding("gpt-2")
 
-        self.data_tokenized = torch.tensor(self.enc.encode(self.data)).to(self.device)
+        self.data_tokenized = torch.tensor(self.simple_encoding(self.data)).to(self.device)
+        print(len(self.data_tokenized))
+        self.model = BigramLanguageModel(self.vocab_size).to(self.device)  # Use tiktoken vocab size, not character vocab!
+        X,y = self.get_batch()
+        print(X,y)
+
+        logits,loss = self.model(X,y)
+        print(f'Loss: {loss}')
+
+        self.optim = torch.optim.AdamW(self.model.parameters())
+
 
         #90-10 train val split
         split = int(self.data_size*0.90)
@@ -35,7 +104,7 @@ class Nano_GPT:
         
     
     def _load_dataset(self,path=None): 
-        data = "input.txt" if path is None else path
+        data = "input.txt" if path is None else path    
         with open(data) as f: 
             self.data = f.read()
         self.data_size = len(self.data)
@@ -59,10 +128,9 @@ class Nano_GPT:
         ## Get one batch of the shakespeare dataset, across two dimensions: Batch dimension and time dimension
         ## One batch is on a random snippet of the data for each batch here
 
-        start_points = torch.randint(0,self.vocab_size-self.block_size,(self.batch_size,))
-        print(start_points)
-        X = torch.stack([self.data_tokenized[i:i+self.block_size] for i in start_points])
-        y = torch.stack([self.data_tokenized[i+1:i+self.block_size+1] for i in start_points])
+        start_points = torch.randint(0,len(self.data_tokenized)-self.block_size,(batch_size,))
+        X = torch.stack([self.data_tokenized[i:i+self.block_size] for i in start_points]).to(self.device)
+        y = torch.stack([self.data_tokenized[i+1:i+self.block_size+1] for i in start_points]).to(self.device)
         return X,y
         
         
@@ -72,16 +140,25 @@ class Nano_GPT:
         enc = tiktoken.get_encoding("o200k_base")
         assert enc.decode(enc.encode("hello world")) == "hello world"
         print(enc.encode("Bharat Jain"))
+    
+    def trainer(self,epochs=10000): 
+        for steps in range(epochs): 
+            xb,yb = self.get_batch()
+            logits, loss = self.model(xb, yb)
+            self.optim.zero_grad(set_to_none=True)
+            loss.backward()
+            self.optim.step()
+        print(loss.item())
 
 
 def main(): 
     gpt = Nano_GPT()
     encoded = gpt.simple_encoding("Bharat Jain")
     print(encoded, "\n", gpt.simple_decoding(encoded))
-    print(gpt.get_batch())
+    print(gpt.simple_decoding(gpt.model.generate(torch.zeros((1,1),dtype=torch.long).to(gpt.device),100)[0].tolist()))
+    gpt.trainer()
+    print(gpt.simple_decoding(gpt.model.generate(torch.zeros((1,1),dtype=torch.long).to(gpt.device),1000)[0].tolist()))
+
+
 if __name__ == "__main__": 
     main()
-
-
-
-
